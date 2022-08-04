adf5ca73f2df1714992a38524790a027a8f6b45e
fiigmnt <fiigmnt@protonmail.com>
2022-08-04 15:10:43 -0400

    fix(wc): removed rainbowkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
+import { WagmiConfig, configureChains, chain, createClient } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
 import "../styles/globals.css";
 
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.rinkeby, chain.goerli],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  connectors: [
+    new InjectedConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+  ],
+});
+
 function Mirage({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
 
 export default Mirage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
 import React from "react";
-import "@rainbow-me/rainbowkit/styles.css";
-import {
-  darkTheme,
-  getDefaultWallets,
-  midnightTheme,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
 import { NextPage } from "next";
-
 import styles from "./index.module.css";
 import Description from "../cards/description/Description";
 import Display from "../cards/display/mobile/Display";
@@ -18,24 +7,8 @@ import Map from "../cards/map/Map";
 import Others from "../cards/others/Others";
 import Modal from "../components/modal/Modal";
 import DesktopDisplay from "../cards/display/desktop/DesktopDisplay";
-import ConnectedUser from "../components/user/ConnectedUser";
 import ConnectButton from "../components/buttons/ConnectButton";
-
 import NoSSR from "../components/utilities/NoSSR";
-
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.rinkeby, chain.goerli],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
-);
-const { connectors } = getDefaultWallets({
-  appName: "Mirage",
-  chains,
-});
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
 
 interface Props {
   isMobileView?: boolean;
@@ -47,45 +20,33 @@ const Home: NextPage<Props> = ({ isMobileView }) => {
   const toggleModal = () => setModalState(!isModalOpen);
 
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={midnightTheme({
-          accentColor: "white",
-          accentColorForeground: "black",
-          borderRadius: "large",
-          fontStack: "system",
-        })}
-      >
-        <NoSSR>
-          {isMobileView ? (
-            <div className={styles.mobileContainer}>
-              <Modal isOpen={isModalOpen} onClose={toggleModal} />
-              <ConnectButton  toggleModal={ toggleModal }/>
-              <Display />
-              <Map isMobileView={isMobileView} />
-              <Description toggleModal={toggleModal}/>
-              <Others isMobileView={isMobileView} />
+    <NoSSR>
+      {isMobileView ? (
+        <div className={styles.mobileContainer}>
+          <Modal isOpen={isModalOpen} onClose={toggleModal} />
+          <ConnectButton toggleModal={toggleModal} />
+          <Display />
+          <Map isMobileView={isMobileView} />
+          <Description toggleModal={toggleModal} />
+          <Others isMobileView={isMobileView} />
+        </div>
+      ) : (
+        <div className={styles.desktopContainer}>
+          <Modal isOpen={isModalOpen} onClose={toggleModal} />
+          <div className={styles.title}>
+            <img src="/images/m.svg" className={styles.titleImage} />
+            <p className={styles.titleText}>
+              DISCOVER BOUNDLESS CREATION BETWEEN CODE AND CONCRETE
+            </p>
+            <div className={styles.connected}>
+              <ConnectButton toggleModal={toggleModal} />
             </div>
-          ) : (
-            <div className={styles.desktopContainer}>
-               <Modal isOpen={isModalOpen} onClose={toggleModal} />
-              <div className={styles.title}>
-                <img src="/images/m.svg" className={styles.titleImage} />
-                <p className={styles.titleText}>
-                  DISCOVER BOUNDLESS CREATION BETWEEN CODE AND CONCRETE
-                </p>
-                <div className={styles.connected}>
-                  <ConnectedUser />
-                </div>
-              </div>
-              <DesktopDisplay toggleModal={toggleModal} />
-              <Others isMobileView={isMobileView} />
-            </div>
-          )}
-        </NoSSR>
-      </RainbowKitProvider>
-    </WagmiConfig>
+          </div>
+          <DesktopDisplay toggleModal={toggleModal} />
+          <Others isMobileView={isMobileView} />
+        </div>
+      )}
+    </NoSSR>
   );
 };
 
