cdf1b68c1f6bdf086318214b4c5b6704a280ff2e
maaxayotte <maxayotte834@gmail.com>
2022-08-04 15:09:33 -0400

    push cause I dont wanna stash

--- flows/connect/ConnectFlow.tsx
+++ flows/connect/ConnectFlow.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./ConnectFlow.module.css";
 
 const ConnectFlow: React.FC = () => {
-  return <div className={styles.container}>
-    <button className={styles.connect}>METAMASK</button>
-    <button className={styles.connect}>WALLETCONNECT</button>
-    <button className={styles.info}>GET A WALLET</button>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.connect}>
+        <p>METAMASK</p> <img src="/images/metamask.svg" />
+      </button>
+      <button className={styles.connect}><p>WALLETCONNECT</p> <img src="/images/walletconnect.png" /></button>
+      <button className={styles.info}>GET A WALLET</button>
+    </div>
+  );
 };
 
 export default ConnectFlow;
