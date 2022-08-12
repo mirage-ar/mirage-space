3852a821f16ff511925ee28bc44e4b0761f8608f
fiigmnt <fiigmnt@protonmail.com>
2022-08-12 13:29:14 -0400

    fix(timer): removed hours mod

--- components/mini/MiniView.tsx
+++ components/mini/MiniView.tsx
@@ -2,48 +2,42 @@ import React from "react";
 import { useApplicationContext } from "../../state/context";
 import { Mirage } from "../../state/types";
 import styles from "./MiniView.module.css";
-import Timer from "../utils/Timer"
+import Timer from "../utils/Timer";
 
 interface MiniProps {
   item: Mirage;
 }
 
-const MobileMini: React.FC<MiniProps> = ({item}) => {
+const MobileMini: React.FC<MiniProps> = ({ item }) => {
   const { isMobileView } = useApplicationContext();
 
-  return isMobileView ? (
-    <div className={styles.container}>
-      <div className={styles.background}>
+  return (
+    <div className={styles[isMobileView ? "container" : "desktopContainer"]}>
+      <div className={styles[isMobileView ? "background" : "desktopBackground"]} >
         <img src="/images/golden_queen.png" />
       </div>
       <div className={styles.pieceInfo}>
-        <div className={styles.pieceName}>{item.name}</div>
-        <div className={styles.artist}>@{item.artist.handle.toUpperCase()}</div>
+        <div className={styles[isMobileView ? "pieceName" : "desktopPieceName"]} >
+          {item.name.toUpperCase()}
+        </div>
+        <div className={styles[isMobileView ? "artist" : "desktopArtist"]}>
+          @{item.artist.handle.toUpperCase()}
+        </div>
         {/* TODO: See below
         <div className={styles.distance}>
           <img src="/images/arrow.png" />
           <p>0.1m</p>
         </div> */}
-      </div>
-    </div>
-  ) : (
-    <div className={styles.desktopContainer}>
-      <div className={styles.desktopBackground}>
-        <img src="/images/golden_queen.png" />
-      </div>
-      <div className={styles.pieceInfo}>
-        <div className={styles.desktopPieceName}>{item.name.toUpperCase()}</div>
-        <div className={styles.desktopArtist}>@{item.artist.handle.toUpperCase()}</div>
-          {/* TODO: Fix this shit or dont use it or whatever
-        <div className={styles.desktopDistance}>
-          <img src="/images/arrow.png" />
-          <p>0.1m</p>
-        </div> */}
         <div className={styles.timer}>
           <img src="/images/liveHex.svg" />
-          <p><Timer start={item.dropStart} end={item.dropEnd}/></p>
+          <p>
+            <Timer
+              start={item.dropStart}
+              end={item.dropEnd}
+            />
+          </p>
         </div>
-        <div className={styles.price}>0.002 ETH</div>
+        {!isMobileView && <div className={styles.price}>0.002 ETH</div>}
       </div>
     </div>
   );
--- components/utils/Timer.tsx
+++ components/utils/Timer.tsx
@@ -16,28 +16,32 @@ const Timer: React.FC<Props> = ({ start, end }) => {
   const [seconds, setSeconds] = useState("00");
   const [minutes, setMinutes] = useState("00");
   const [hours, setHours] = useState("00");
-  let timer;
+
+  const [timer, setTimer] = useState(null);
 
   useEffect(() => {
     const today = new Date();
-    if (start > today) {
-      timeBetweenDates(start, end);
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    if (startDate > today) {
+      timeBetweenDates(startDate, endDate);
     } else {
-      timer = setInterval(function () {
-        timeBetweenDates(start, end);
-      }, 1000);
+      setTimer(
+        setInterval(function () {
+          timeBetweenDates(startDate, endDate);
+        }, 1000)
+      );
     }
   }, []);
 
   function timeBetweenDates(start, end) {
-    const rightNow = new Date();
-    const startTime = new Date(start).getTime();
-    const endTime = new Date(end).getTime();
+    const rightNow = new Date().getTime();
+    const startTime = start.getTime();
+    const endTime = end.getTime();
 
     const difference =
-      start > rightNow
-        ? endTime - startTime
-        : endTime - rightNow.getTime();
+      start > rightNow ? endTime - startTime : endTime - rightNow;
 
     if (difference <= 0) {
       // timer finished
@@ -49,7 +53,7 @@ const Timer: React.FC<Props> = ({ start, end }) => {
       //   var days = Math.floor(hours / 24); - no days included in timer
 
       // calculate mods
-      hours %= 24;
+      // hours %= 24; // not using days, don't mod hours
       minutes %= 60;
       seconds %= 60;
 
