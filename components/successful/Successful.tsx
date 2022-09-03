b91730be6a826d3649311df2d06e8018634447af
fiigmnt <fiigmnt@protonmail.com>
2022-09-03 18:24:34 -0400

    added disconnect button

--- components/successful/Successful.tsx
+++ components/successful/Successful.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useAccount, useDisconnect } from "wagmi";
 import Address from "../utils/Address";
 import styles from "./Successful.module.css";
 
 const Successful: React.FC = () => {
+  const { isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+
   return (
     <div className={styles.container}>
       <div className={styles.button}>
         <img src="/images/check.svg" />
       </div>
-      <p className={styles.connectText}>Connected as <Address /></p>
+      <p className={styles.connectText}>
+        Connected as <Address /> |{" "}
+        {isConnected && <a style={{cursor: "pointer"}} onClick={() => disconnect()}>Disconnect</a>}
+      </p>
     </div>
   );
 };
