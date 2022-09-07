00640b2f97e5926fe852f2503fc0b8c40dd5caa6
maaxayotte <maxayotte834@gmail.com>
2022-09-07 15:14:30 -0400

    experience viewer added

--- components/model/ExperienceViewer.js
+++ components/model/ExperienceViewer.js
@@ -1,8 +1,9 @@
 import "@google/model-viewer";
 
 const ExperienceViewer = ({ contractAddress }) => (
-  <div id="card">
+  <div id="experienceCard">
     <model-viewer
+        id="experience"
       src={`/experience/models/${contractAddress}.glb`}
       ios-src={`/experience/models/${contractAddress}.usdz`}
       poster={`/experience/images/${contractAddress}.png`}
--- pages/experience/[contract].tsx
+++ pages/experience/[contract].tsx
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
-import ExperienceViewer from "../../components/model/ExperienceViewer";
+import dynamic from 'next/dynamic'
+import styles from "./experience.module.css"
+
+const ExperienceViewer = dynamic(
+    () => import('../../components/model/ExperienceViewer'),
+    { ssr: false }
+  )
 
 const Experience: React.FC = () => {
   const router = useRouter();
   const { contract } = router.query;
 
   return (
-    <div>
+    <div className={styles.container}>
       <ExperienceViewer contractAddress={contract} />
     </div>
   );
