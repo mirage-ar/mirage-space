1b0a7daabcca3404dfb816ce14b86018e85b5885
maaxayotte <maxayotte834@gmail.com>
2022-09-07 16:00:15 -0400

    blapple

--- components/model/ExperienceViewer.js
+++ components/model/ExperienceViewer.js
@@ -6,7 +6,7 @@ const ExperienceViewer = ({ contractAddress }) => (
         id="experience"
       src={`/experience/models/${contractAddress}.glb`}
       ios-src={`/experience/models/${contractAddress}.usdz`}
-      poster={`/experience/images/${contractAddress}.png`}
+    //   poster={`/experience/images/${contractAddress}.png`}
       alt="Mirage 3D Model"
       camera-controls
       auto-rotate
