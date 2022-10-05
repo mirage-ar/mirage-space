3038f7d918ebb9d68852a072dd9546a99627c91e
maaxayotte <maxayotte834@gmail.com>
2022-10-05 12:34:42 -0400

    More breakpoints for rsvp and about page

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -5,57 +5,53 @@ import styles from "./rsvp.module.css";
 import NoSSR from "../components/utils/NoSSR";
 import Link from "next/link";
 
-const Rsvp: NextPage= () => {
+const Rsvp: NextPage = () => {
   return (
     <NoSSR>
-        <div className={styles.container}>
-          <div>
-            <p className={styles.topText}>
-              Limited EDITION, Unlimited POSSIBILITY
+      <div className={styles.container}>
+        <div className={styles.topTextContainer}>
+          <p className={styles.topText}>Limited EDITION,</p>
+          <p className={styles.topText}>Unlimited POSSIBILITY</p>
+        </div>
+        <div className={styles.infoCard}>
+          <div className={styles.cardTitle}>
+            <img src="/images/M.png" />
+            <p className={styles.inviteText}>
+              <p>SOMEWHERE BETWEEN CODE AND CONCRETE IS A CANVAS.</p>
+              <p>ON TUESDAY, OCTOBER 25, 2022, PROSPECT PARK,</p>
+              <p>THAT CANVAS WILL BE ACTIVATED,</p>
+              <p>AND YOU'RE INVITED.</p>
             </p>
           </div>
-          <div className={styles.infoCard}>
-            <div className={styles.cardTitle}>
-              <img src="/images/M.png" />
-              <p className={styles.inviteText}>
-                <p>SOMEWHERE BETWEEN CODE AND CONCRETE IS A CANVAS.</p>
-                <p>ON TUESDAY, OCTOBER 25, 2022, PROSPECT PARK,</p>
-                <p>THAT CANVAS WILL BE ACTIVATED,</p>
-                <p>AND YOU'RE INVITED.</p>
-              </p>
-            </div>
-            <div className={styles.bottomText}>
-              <div className={styles.leftText}>
-                <div className={styles.times}>
-                  <p>3:00</p>
-                  <p>3:15</p>
-                  <p>3:30</p>
-                  <p>4:30</p>
-                </div>
-                <div className={styles.activities}>
-                  <p>ARRIVAL</p>
-                  <p>OPENING REMARKS</p>
-                  <p>TREASURE HUNT</p>
-                  <p>DJ SETS BY...</p>
-                </div>
+          <div className={styles.bottomText}>
+            <div className={styles.leftText}>
+              <div className={styles.times}>
+                <p>3:00</p>
+                <p>3:15</p>
+                <p>3:30</p>
+                <p>4:30</p>
               </div>
-              <div className={styles.rsvpContainer}>
-                <button className={styles.rsvp}>
-                  <p>I WILL COME</p>
-                </button>
+              <div className={styles.activities}>
+                <p>ARRIVAL</p>
+                <p>OPENING REMARKS</p>
+                <p>TREASURE HUNT</p>
+                <p>DJ SETS BY...</p>
               </div>
             </div>
-          </div>
-          <div className={styles.bottomTitle}>
-            <p className={styles.bottomTitle}>MIRAGE IS DROPPING SOON</p>
-            <Link href={"https://beta.mirage.space/about"}>
-              <button className={styles.bottomButton}>
-                {" "}
-                MORE ABOUT MIRAGE
+            <div className={styles.rsvpContainer}>
+              <button className={styles.rsvp}>
+                <p>I WILL COME</p>
               </button>
-            </Link>
+            </div>
           </div>
         </div>
+        <div className={styles.bottomTitle}>
+          <p className={styles.bottomTitle}>MIRAGE IS DROPPING SOON</p>
+          <Link href={"https://beta.mirage.space/about"}>
+            <button className={styles.bottomButton}> MORE ABOUT MIRAGE</button>
+          </Link>
+        </div>
+      </div>
     </NoSSR>
   );
 };
